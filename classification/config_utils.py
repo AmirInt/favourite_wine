03bcf5bc51497fdc0995c7ccc20db241d92388db
AmirInt <amir.intelli@gmail.com>
2023-12-16 11:01:04 +0000

    Fix issues with utils and complete the config file options.

--- classification/config_utils.py
+++ classification/config_utils.py
@@ -1,5 +1,8 @@
 import yaml
 
+
+
+
 def load_config() -> dict:
     with open("conf.yaml") as conf_file:
         try:
--- classification/dataset_utils.py
+++ classification/dataset_utils.py
@@ -13,19 +13,19 @@ class Dataset:
             features: list,
             labels: list) -> None:
 
-        self._data = np.loadtxt(data_path)
+        self._data = np.loadtxt(data_path, delimiter=',')
         self._train_count = train_count
         self._features = features
         self._labels = labels
 
 
-    def prepare_dataset() -> None:
+    def prepare_dataset(self) -> None:
         np.random.seed(0)
         perm = np.random.permutation(self._data.shape[0])
-        self._trainx = self._data[perm[0:self._train_count, 1:14]]
-        self._trainy = self._data[perm[0:self._train_count, 0]]
-        self._testx = self._data[perm[self._train_count:self._data.shape[0], 1:14]]
-        self._testy = self._data[perm[self._train_count:self._data.shape[0], 0]]
+        self._trainx = self._data[perm[0:self._train_count], 1:14]
+        self._trainy = self._data[perm[0:self._train_count], 0]
+        self._testx = self._data[perm[self._train_count:self._data.shape[0]], 1:14]
+        self._testy = self._data[perm[self._train_count:self._data.shape[0]], 0]
 
 
     def get_features(self) -> list:
@@ -34,3 +34,19 @@ class Dataset:
     
     def get_labels(self) -> list:
         return self._labels
+    
+
+    def get_trainx(self) -> np.ndarray:
+        return self._trainx
+    
+    
+    def get_trainy(self) -> np.ndarray:
+        return self._trainy
+    
+    
+    def get_testx(self) -> np.ndarray:
+        return self._testx
+    
+    
+    def get_testy(self) -> np.ndarray:
+        return self._testy
