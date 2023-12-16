0688e0788334f0f959e3d0abe9d9dfa3cf36f6d8
AmirInt <amir.intelli@gmail.com>
2023-12-16 09:56:00 +0000

    Add modules for dataset preparation and config file loading.

---
+++ classification/dataset_utils.py
@@ -0,0 +1,36 @@
+import os
+import numpy as np
+import random
+
+
+
+
+class Dataset:
+    def __init__(
+            self,
+            data_path: os.PathLike,
+            train_count: int,
+            features: list,
+            labels: list) -> None:
+
+        self._data = np.loadtxt(data_path)
+        self._train_count = train_count
+        self._features = features
+        self._labels = labels
+
+
+    def prepare_dataset() -> None:
+        np.random.seed(0)
+        perm = np.random.permutation(self._data.shape[0])
+        self._trainx = self._data[perm[0:self._train_count, 1:14]]
+        self._trainy = self._data[perm[0:self._train_count, 0]]
+        self._testx = self._data[perm[self._train_count:self._data.shape[0], 1:14]]
+        self._testy = self._data[perm[self._train_count:self._data.shape[0], 0]]
+
+
+    def get_features(self) -> list:
+        return self._features
+
+    
+    def get_labels(self) -> list:
+        return self._labels
